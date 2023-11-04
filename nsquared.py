6b9f365d3259ca0216831e73b4215ef0b47fe975
kelvin <kelvinhome2121@gmail.com>
2023-11-04 14:14:59 +0300

    added a function for computing factorial and the mergesort algorithm but has errors

--- nsquared.py
+++ nsquared.py
@@ -1,10 +1,32 @@
-def nlogn(n):
-    y = n
-    while n > 1:
-         n = n//2
-         i = 1
-         while i <= y:
-               print(i)
-               i += 1
-               
-nlogn(8)
+def mergesort(array):
+     if len(array)<2:
+         return array
+     middleindex = len(array)//2
+     leftarray = array[:middleindex]
+     rightarray = array[middleindex:]
+     return merge(mergesort(leftarray),mergesort(rightarray))
+
+
+def merge(leftarray , rightarray):
+     result  = []
+     leftIndex = 0
+     rightIndex  = 0
+
+     while (leftIndex < len(leftarray) and rightIndex < len(rightarray)):
+          if leftarray[leftIndex] < rightarray[rightIndex]:
+               result.append(leftarray[leftIndex])
+               leftIndex += 1
+          else:
+               result.append(rightarray[rightIndex])
+               rightIndex += 1
+
+def fact(n):
+     start = 1
+     if n == 0:
+          return 1
+     else:
+          for i in range(1,n+1):
+               start *= i
+          return start
+
+print(fact(2))
